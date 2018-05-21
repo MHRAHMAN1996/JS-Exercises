18ba1899a65c3c251e68a5e333b35a119553e465
Md Hasibur Rahman <mhrahman1996@gmail.com>
2018-05-21 04:43:11 +0000

    master

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ function sumArray(arr){
 }
 console.log(sumArray([1,2.3,4,5]));
 console.log(sumArray([1,20,-3,45]));
+
+//Task 3
+function checkEmail(emailString){
+    var emailFormat= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var match =emailFormat.test(emailString);
+    return match;
+}
+console.log(checkEmail("jon@smith.com"));
+console.log(checkEmail("jon@smith.com"))
+
+
+
+
